797e73d445a3f9603e1963df44f68be1bcc8e324
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-06-15 12:12:47 +0530

    edit functionality also done

--- app.js
+++ app.js
@@ -21,12 +21,46 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/profile", isLoggedIn, async(req, res) => {
- let userr = await userModel.findOne({email: req.user.email})
- console.log(userr);
- 
-    res.render("profile", {userr});
+let user = await userModel.findOne({email: req.user.email}).populate("posts")
+res.render("profile", {user});
 });
 
+app.get("/like/:id", isLoggedIn, async(req, res) => {
+let post = await postModel.findOne({_id: req.params.id}).populate("user");
+
+if(post.likes.indexOf(req.user.userid) === -1){
+  post.likes.push(req.user.userid)
+}
+else{
+ post.likes.splice(post.likes.indexOf(req.user.userid), 1) 
+}
+await post.save()
+res.redirect("/profile");
+});
+
+
+app.get("/edit/:id", isLoggedIn, async(req, res) => {
+let post = await postModel.findOne({_id: req.params.id}).populate("user");
+ res.render("edit", {post})
+});
+
+app.post("/update/:id", isLoggedIn, async(req, res) => {
+let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+ res.redirect("/profile")
+});
+
+
+app.post("/post", isLoggedIn, async(req, res) => {
+ let user = await userModel.findOne({email: req.user.email})
+ let {content} = req.body
+let post = await postModel.create({
+  user: user._id,
+  content 
+})
+user.posts.push(post._id)
+await user.save();
+res.redirect("/profile")
+});
 
 app.post("/register", async (req, res) => {
   let { email, password, username, name, age } = req.body;
@@ -61,7 +95,7 @@ app.post("/login", async (req, res) => {
     if (result) {
         let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
         res.cookie("token", token);
-      res.status(200).send("/profile");
+      res.status(200).redirect("/profile");
     } 
     else res.redirect("/login");
   });
